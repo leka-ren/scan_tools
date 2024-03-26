275bee930aee263e6bc09858fd02340bfc99f0f7
Polina Ren <56325631+leka-ren@users.noreply.github.com>
2024-03-26 11:40:34 +0800

    connects worker with channel

--- tcp_port_scanner.go
+++ tcp_port_scanner.go
@@ -6,10 +6,27 @@ import (
 	"sync"
 )
 
+var host string
+var network string
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for port := range ports {
+		address := fmt.Sprintf("%s:%d", host, port)
+
+		conn, err_net_dial := net.Dial(network, address)
+
+		if err_net_dial != nil {
+			continue
+		}
+
+		conn.Close()
+		fmt.Printf("Connection success for port: %d\n", port)
+		wg.Done()
+	}
+}
+
 func main() {
 	var wg sync.WaitGroup
-	var host string
-	var network string
 
 	fmt.Println("Hello! It's TCP-client for port scrinning.")
 
@@ -27,21 +44,17 @@ func main() {
 
 	fmt.Printf("Scan for %s start...\n", host)
 
+	ports := make(chan int, 100)
+
+	for channel := 0; channel < cap(ports); channel++ {
+		go worker(ports, &wg)
+	}
+
 	for port := 1; port <= 65535; port++ {
 		wg.Add(1)
-		go func(port_p int) {
-			defer wg.Done()
-			address := fmt.Sprintf("%s:%d", host, port_p)
-
-			conn, err_net_dial := net.Dial(network, address)
-
-			if err_net_dial != nil {
-				return
-			}
-
-			conn.Close()
-			fmt.Printf("Connection success for port: %d\n", port_p)
-		}(port)
+		ports <- port
 	}
+
 	wg.Wait()
+	close(ports)
 }
